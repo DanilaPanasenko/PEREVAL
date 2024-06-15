41f70039bd108dc386488f9c76c8ff3eca2d746e
Данила <danilapanasenko0@mail.ru>
2024-06-15 21:40:44 +0300

    commit 6

--- pereval/pereval/yasg.py
+++ pereval/pereval/yasg.py
@@ -11,11 +11,11 @@ schema_view = get_schema_view(
         license=openapi.License(name='BSD License'),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny),
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.without_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
